e710bf440e4ec2fb7cc36288ca64b49a1d9a6231
haha861924 <qq861924@gmail.com>
2021-11-08 15:20:14 +0800

    feat: 新增 navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-grey-200 w-full h-16">
+    hello
+    <IconButton :text="'想要去哪?'" />
+  </div>
+</template>
+<script>
+import IconButton from './IconButton.vue'
+export default {
+    name: 'Navbar',
+    components: { IconButton }
+}
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Home!!</h1>
+  <div>
+    <Navbar />
+  </div>
 </template>
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
     name: 'HomePage',
+    components: { Navbar },
     created () {
         console.log('test hello')
     }
