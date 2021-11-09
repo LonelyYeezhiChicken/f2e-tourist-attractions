55980c7bf7a16e1747f08901c593481be2de14bb
haha861924 <qq861924@gmail.com>
2021-11-10 05:37:55 +0800

    feat: 新增路徑導向

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,21 @@ const routes = [
       path: "/",
       name: "Home",
       component: () => import("../views/HomePage.vue"),
+    },
+    {
+      path: "/tour",
+      name: "Tour",
+      component: () => import("../views/TourPage.vue"),
+    },
+    {
+      path: "/restaurant",
+      name: "Restaurant",
+      component: () => import("../views/RestaurantPage.vue"),
+    },
+    {
+      path: "/bed",
+      name: "Bed",
+      component: () => import("../views/BedPage.vue"),
     }
   ];
   
---
+++ src/views/RestaurantPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hello, restaurant</h1>
+</template>
