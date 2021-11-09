ba3c7e688d4de062437167a101d37e1696e69e33
haha861924 <qq861924@gmail.com>
2021-11-10 03:22:20 +0800

    feat: 新增 navbar 搜尋框

---
+++ src/components/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="relative">
+        <label for="search" class="absolute mt-2 ml-5">{{label}}</label>
+        <input name="search" class="p-2 pl-2 w-80 rounded-3xl ">
+    </div>
+</template>
+<script>
+export default {
+    name: 'icon-input',
+    props: {label: { type: String, required: true }}
+}
+</script>
