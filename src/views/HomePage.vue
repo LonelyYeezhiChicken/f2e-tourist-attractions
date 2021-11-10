c3eb61397b726cf4bda7d6ce1430c3fd6e3a8cd3
haha861924 <qq861924@gmail.com>
2021-11-11 00:35:41 +0800

    feat: SPA

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,22 +1,25 @@
 <template>
-  <div>
+  <div class="flex flex-col h-screen justify-between">
     <Navbar />
-    <GoogleMap />
-    <div>
+    <router-view></router-view>
+    <!-- <GoogleMap /> -->
+    <!-- <div>
       <SvgIcon class="search" name="search" width="2rem" height="2rem"/>
       <SvgIcon name="tour" />
       <SvgIcon name="brand" />
-    </div>
+    </div> -->
+    <Footer />
   </div>
 </template>
 <script>
 import Navbar from '../components/Navbar.vue'
-import GoogleMap from '../components/GoogleMap.vue'
+import Footer from '../components/Footer.vue'
+// import GoogleMap from '../components/GoogleMap.vue'
 // import { cityScenicSpot } from '@/api/Api'
 
 export default {
   name: 'HomePage',
-  components: { Navbar, GoogleMap },
+  components: { Navbar, Footer },
   created() {
     // cityScenicSpot('NewTaipei', { $top: 10 })
   },
