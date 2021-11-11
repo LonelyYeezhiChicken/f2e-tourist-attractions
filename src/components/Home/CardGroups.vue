cf9151cc91b670241da8bad555165565260fe475
haha861924 <qq861924@gmail.com>
2021-11-12 03:03:59 +0800

    feat: 卡片組

---
+++ src/components/Home/CardGroups.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full bg-white h-48 px-20">
+    <div class="flex justify-between font-bold mb-5">
+        <span class="text-xl text-black">{{title}}</span>
+        <button class="rounded-3xl py-2 px-4 bg-primary-700 text-grey-100">查看更多</button>
+    </div>
+    <div class="flex justify-center">
+      <Card v-for="card in 4" :key="card" />
+    </div>
+  </div>
+</template>
+<script>
+import Card from '../Card.vue'
+export default {
+  name: 'cardGroups',
+  props: { title: { type: String } },
+  components: { Card },
+}
+</script>
