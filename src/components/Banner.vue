38187d4cd68f7d9f584be3b825f5179693cf0887
haha861924 <qq861924@gmail.com>
2021-11-13 20:41:41 +0800

    feat: 新增景點、飯店、餐廳的 banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-60 relative">
+      <div class="absolute font-extrabold text-5xl text-grey-100 ml-64 mt-14">{{title}}</div>
+      <img :src="`src/assets/${imgurl}.png`" class="w-full">
+  </div>
+</template>
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    imgurl: { type: String },
+    title: { type: String }
+  },
+  created () {
+      console.log(this.imgurl)
+  }
+}
+</script>
--- src/views/BedPage.vue
+++ src/views/BedPage.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>hello, bed</h1>
+  <div class="h-auto">
+        <Banner :imgurl="'bed-banner'" title="飯店列表"/>
+    </div>
 </template>
+<script>
+import Banner from '@/components/Banner.vue'
+export default {
+  name: 'BedPage',
+  components: { Banner }
+}
+</script>
